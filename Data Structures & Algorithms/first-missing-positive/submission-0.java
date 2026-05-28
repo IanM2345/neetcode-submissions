adbc3070a066fc5f478597b168416ebb3e64f8ee
Ian Mwai Gachoki <gachokiian63@gmail.com>
2026-05-28 12:25:47 +0100

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int exempr = 1;
+
+        while(true){
+            boolean flag = true;
+            for(int num: nums){
+                if(num == exempr){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                return exempr;
+            }
+            exempr++;
+        }
+    }
+}
