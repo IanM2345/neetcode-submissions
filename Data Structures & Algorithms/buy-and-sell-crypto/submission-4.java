068f37b6376991361973cfe8c8d30f225ae577b4
Ian Mwai Gachoki <gachokiian63@gmail.com>
2026-05-28 11:32:41 +0100

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int sum = 0;
+
+        while(right<prices.length){
+            if(prices[left]<prices[right]){
+                sum = Math.max(prices[right] - prices[left], sum);
+            } else{
+                left = right;
+            }
+            right++;
+        }
+        return sum;
+    }
+}
