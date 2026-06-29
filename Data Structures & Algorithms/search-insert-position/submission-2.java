1cebb802b5eb489e44d2e8cb99c5acb6e82365ee
Ian Mwai Gachoki <gachokiian63@gmail.com>
2026-06-29 09:21:43 +0100

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int res = nums.length;
+
+        while (left <= right) {
+            int n = (right + left) / 2;
+
+            if (nums[n] == target) {
+                return n;
+            }
+
+            if (nums[n] > target) {
+                res = n;
+                right = n - 1;
+            } else {
+                left = n + 1;
+            }
+        }
+
+        return res;
+    }
+}
