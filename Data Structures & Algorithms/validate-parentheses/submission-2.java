d5453ead844f0da4a0506fbd45e1c659b88321cd
Ian Mwai Gachoki <gachokiian63@gmail.com>
2026-07-08 14:07:12 +0100

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(char c:s.toCharArray()){
+            if(st.isEmpty()&&(c==')'||c==']'||c=='}'))
+            return false;
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
+                continue;
+            }else{
+                if(!st.isEmpty()){
+                    if(c==')' && st.peek()=='('){
+                        st.pop();
+                    }
+                    else if(c=='}' && st.peek()=='{'){
+                        st.pop();
+                    }
+                    else if(c==']' && st.peek()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
