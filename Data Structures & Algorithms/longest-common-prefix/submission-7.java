a3fca4440bc2d9fa7d027a814d267b7de58333df
Ian Mwai Gachoki <gachokiian63@gmail.com>
2026-07-22 11:36:30 +0100

    Add: longest-common-prefix - submission-7

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-7.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        Arrays.sort(strs);
+        int n = Math.min(strs[0].length(), strs[strs.length - 1].length());
+        for (int i = 0; i < n; i++) {
+            for (String s : strs) {
+                if (s.length() == i || s.charAt(i) != strs[0].charAt(i)) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
